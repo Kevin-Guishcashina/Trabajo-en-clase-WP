4fb5e6c6741609b4e5a2e290799532ef57929aa7
Kevin-Guishcashina <kevinguishcashinaxd@gamil.com>
2021-05-26 19:57:40 -0500

    Tarea de una pagina de mi perfil

--- front-page.php
+++ front-page.php
@@ -1,44 +1,54 @@
 <?php get_header(); ?>
-<main content-center>
-    <span class="text-2xl">PRESENTACION</span>
-    <div class="flex justify-center space-x-10  ">
+<main>
 
-        <div class="w-2/12">
-            <img class="rounded-full" src="<?php echo get_template_directory_uri() ?>/assets/img/perfil.jpg" olt="imagen de perfil">
-        </div>
-        <div class="w-6/12">
-            <p>Mi nombre es Kevin Guishcashina me gusta el diseño web.
-                Actualmente estoy cursando el sexto nivel de la carrera de Análisis de Sistemas en el Instituto Tecnológico Quito
-                Me gusta la carrera ya que me quiero dedicar al diseño web
 
-            </p>
-        </div>
-    </div>
-    <div class="text-2xl">Me gusta trabajar con estos lenguajes</div>
-    <div class="grid grid-cols-3 gap-4">
-        <div>
-            <img class="rounded-full" src="<?php echo get_template_directory_uri() ?>/assets/img/450.jpg" olt="imagen de perfil">
-            <p>JAVA porque permite crear diferentes aplicaciones y procesos en una gran variedad de dispositivos </p>
+    <div>
+        <div class="max-w-md mx-auto bg-gray-100 rounded-xl shadow-md overflow-hidden md:max-w-5xl mt-6">
+            <div class="text-4xl text-center">PRESENTACION</div>
+            <div class="md:flex">
+                <div class="md:flex-shrink-0">
+                    <img class="h-48 w-full object-cover md:h-full md:w-48" src="<?php echo get_template_directory_uri() ?>/assets/img/perfil.jpg" alt="Mi foto">
+                </div>
+                <div class="p-8">
+                    <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Mi Perfil</div>
+                    <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Mi nonmbre es Kevin Guishcashina</a>
+                    <p class="mt-2 text-gray-500">Actualmente estudio la carrera de Análisis de Sistemas en el Instituto Tecnológico Quito
+                        6to nivel.
+                    </p>
+                </div>
+            </div>
         </div>
 
-        <div>
-            <img class="rounded-full" src="<?php echo get_template_directory_uri() ?>/assets/img/css.png" olt="imagen de perfil">
-            <p>CSS porque me gusta la manera en la que puedo dar formato y diseño a nuestro sitio web</p>
+
+        <div class="max-w-md mx-auto bg-gray-50 rounded-xl shadow-md overflow-hidden md:max-w-5xl mt-6">
+            <div class="uppercase tracking-wide text-sm text-indigo-1000 font-semibold text-2xl">Me gusta trabajar con:</div><br>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div class="bg-blue-50">
+                    <img src="<?php echo get_template_directory_uri() ?>/assets/img/450.jpg" alt="imagen de perfil">
+                    <p>JAVA porque permite crear diferentes aplicaciones y procesos en una gran variedad de dispositivos </p>
+                </div>
+                <div class="bg-blue-50">
+                    <img class="" src="<?php echo get_template_directory_uri() ?>/assets/img/css.png" alt="imagen de perfil">
+                    <p>CSS porque me gusta la manera en la que puedo dar formato y diseño a nuestro sitio web</p>
+                </div>
+                <div class="bg-blue-50">
+                    <img class="" src="<?php echo get_template_directory_uri() ?>/assets/img/php.png" alt="imagen de perfil">
+                    <p>PHP porque es un lenguaje de programacion que se adapta muy bien al desarrollo web</p>
+                </div>
+            </div>
+
+            
+        <div class="flex justify-center md:max-w-5xl mt-10">
+        <div class="mb-10">
+        <h1 >Pasatiempos</h1>
+        
         </div>
-        <div>
-            <img class="rounded-full" src="<?php echo get_template_directory_uri() ?>/assets/img/php.png" olt="imagen de perfil">
-            <p>PHP porque es un lenguaje de programacion que se adapta muy bien al desarrollo web</p>
+
+            
         </div>
+        </div>
+
     </div>
-    <div class="text-2xl flex justify-center ">Pasatiempos</div>
-    <div class="flex justify-center space-x-10 ">
-        <div class="w-2/12">
-        <img class="rounded-full" src="<?php echo get_template_directory_uri() ?>/assets/img/guitar.jpg" olt="imagen de perfil">
-        </div>
-        <div class="w-4/12">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, autem? Facilis optio pariatur in dolorem, quod accusamus dolorum dolores eaque expedita ex exercitationem, nihil, laudantium assumenda neque tempora delectus dolore?</p>
-        </div>
-    </div>
-</div>
+
 </main>
 <?php get_footer(); ?>
